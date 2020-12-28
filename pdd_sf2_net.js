747db39d16b7f0d9b33e3d14a7f278ffad8b7fa3
fidcz <fidcz123@163.com>
2020-12-28 09:44:16 +0800

    update pdd_sf2_net.js. 添加更新链接

--- pdd_sf2_net.js
+++ pdd_sf2_net.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         拼多多审方-通过
 // @namespace    pdd-sf-tg
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       fidcz
 // @match        *mms.pinduoduo.com/orders/medicine/detail?orderSn*
 // @grant        none
+// @updateURL    https://gitee.com/fidcz/blyy_tools_update/raw/master/pdd_sf_net.js
 // ==/UserScript==
 
 (function() {
