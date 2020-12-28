9adc33d9c1579dd8b73da1a63941b94e5b602f52
fidcz <fidcz123@163.com>
2020-12-28 09:42:35 +0800

    add pdd_sf2_net.js. 拼多多审方通过

---
+++ pdd_sf2_net.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         拼多多审方-通过
+// @namespace    pdd-sf-tg
+// @version      0.1
+// @description  try to take over the world!
+// @author       fidcz
+// @match        *mms.pinduoduo.com/orders/medicine/detail?orderSn*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    // 延迟
+    function sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+// 主运行
+    async function run(){
+        //主运行
+        while (document.querySelector("i[data-testid='beast-core-icon-time-circle_filled']") == null) {
+            await sleep(1000);
+            if (document.querySelector("i[data-testid='beast-core-icon-time-circle_filled']") != null){
+                //console.log("小框框藏起来了")
+                break;
+            }
+        }
+
+
+        if(document.querySelector("i[data-testid='beast-core-icon-time-circle_filled']").parentNode.querySelector("span").innerText == "待审核"){
+            // 待审核
+            while (document.querySelector("button.BTN_outerWrapper_4-103-1") == null) {
+                await sleep(1000);
+                if (document.querySelector("button.BTN_outerWrapper_4-103-1") != null){
+                    //console.log("小框框藏起来了")
+                    break;
+                }
+            }
+
+            document.querySelectorAll("button.BTN_outerWrapper_4-103-1")[0].click()
+            await sleep(1500);
+
+
+        }
+        window.close();
+
+
+        // 主运行结束
+    }
+
+    run();
+})();
