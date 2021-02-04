c775e2a315da7f0dcde09958d225a587582a98dd
fidcz <fidcz123@163.com>
2021-02-04 19:27:04 +0800

    添加获取ClassNumber，并修改更新URL，Ver0.3

--- 审方脚本/pdd_sf_net.js
+++ 审方脚本/pdd_sf_net.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         拼多多审方
 // @namespace    pdd-sf1
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        *mms.pinduoduo.com/orders/medicine
