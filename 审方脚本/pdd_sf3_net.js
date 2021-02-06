ac447e6b6a550eb1dbb8d2487797394a2d5076e2
fidcz <fidcz123@163.com>
2021-02-06 15:02:07 +0800

    update 审方脚本/pdd_sf3_net.js.

--- 审方脚本/pdd_sf3_net.js
+++ 审方脚本/pdd_sf3_net.js
@@ -20,16 +20,21 @@
         window.focus();
 
     document.querySelector("div.last-item").click();
-
+        console.log("点击账号密码");
+    await sleep(800);
+        window.focus();
+    document.getElementById("usernameId").click();
 
                 while (document.getElementById("usernameId").value == "") {
             await sleep(1000);
             if (document.getElementById("usernameId").value != ""){
-                //console.log("小框框藏起来了")
+                console.log("小框框藏起来了")
                 break;
             }
         }
+        window.focus();
         document.querySelector("button[data-testid='beast-core-button']").click();
+        console.log("点击登录");
         //document.querySelector("button.BTN_outerWrapperBtn_4-61-1").click();
         await sleep(3000);
         location.reload();
