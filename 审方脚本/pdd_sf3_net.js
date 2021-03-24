889b2e32b6e3052a96c9cb447892e8d235285be4
fidcz <fidcz123@163.com>
2021-03-24 11:46:26 +0800

    update 审方脚本/pdd_sf3_net.js.

--- 审方脚本/pdd_sf3_net.js
+++ 审方脚本/pdd_sf3_net.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name         拼多多重新登录
+// @name         拼多多掉线提醒
 // @namespace    pdd-sf3
 // @version      0.1
 // @description  try to take over the world!
 // @author       fidcz
 // @match        https://mms.pinduoduo.com/login*
 // @grant        window.focus
-// @updateURL    https://gitee.com/fidcz/blyy_tools_update/raw/master/%E5%AE%A1%E6%96%B9%E8%84%9A%E6%9C%AC/pdd_sf3_net.js
+// @grant        GM_xmlhttpRequest
+// @connect      192.168.0.29
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var ip_port = "192.168.0.29:5000"
 
         function sleep(time) {
         return new Promise((resolve) => setTimeout(resolve, time));
@@ -18,26 +20,14 @@
 
     async function run(){
         window.focus();
-
-    document.querySelector("div.last-item").click();
-        console.log("点击账号密码");
-    await sleep(800);
-        window.focus();
-    document.getElementById("usernameId").click();
-
-                while (document.getElementById("usernameId").value == "") {
-            await sleep(1000);
-            if (document.getElementById("usernameId").value != ""){
-                console.log("小框框藏起来了")
-                break;
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: "http://" + ip_port + "/send/拼多多菠萝审方掉线了!",
+            timeout: 2000,
+            onload: (response) => {
+                console.log(response.status)
             }
-        }
-        window.focus();
-        document.querySelector("button[data-testid='beast-core-button']").click();
-        console.log("点击登录");
-        //document.querySelector("button.BTN_outerWrapperBtn_4-61-1").click();
-        await sleep(3000);
-        location.reload();
+        });
     }
 
 
