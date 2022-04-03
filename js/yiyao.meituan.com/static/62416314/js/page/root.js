edf3bf4a8e68a5b0af80865512c4cf698f15612e
fidcz <fidcz123@163.com>
2022-04-03 16:18:48 +0800

    更新文件 root.js

--- js/yiyao.meituan.com/static/62416314/js/page/root.js
+++ js/yiyao.meituan.com/static/62416314/js/page/root.js
@@ -5463,7 +5463,7 @@ define('module/root/left_nav', ["module/cookie", "module/root/audio", "module/ro
         e.removeClass("hide"),
         a.forEach(function(a, e) {
             '订单管理' === a.menuName ? a.subMenus.push({interfaceCode: "", limitInfo: "", mask: "new", menuCode: "Sub.OrderHistory", menuIcon: null, pageId: null, permissionId: 0, rank: "12", subMenuName: "历史订单(旧)", url: "/v2/order/history"}) : !0;
-            '订单管理' === a.menuName ? a.subMenus.push({interfaceCode: "", limitInfo: "", mask: "new", menuCode: "Sub.RefundOrder", menuIcon: null, pageId: null, permissionId: 0, rank: "13", subMenuName: "退款", url: "/v2/order/refund/unprocessed"}) : !0;
+            '订单管理' === a.menuName ? a.subMenus.push({interfaceCode: "", limitInfo: "", mask: "new", menuCode: "Sub.RefundOrder", menuIcon: null, pageId: null, permissionId: 0, rank: "9", subMenuName: "退款", url: "/v2/order/refund/unprocessed"}) : !0;
             '订单管理' === a.menuName ? a.subMenus.push({interfaceCode: "", limitInfo: "", mask: "new", menuCode: "Sub.OrderReminder", menuIcon: null, pageId: null, permissionId: 0, rank: "14", subMenuName: "催单", url: "/v2/order/reminder"}) : !0;
             '订单管理' === a.menuName ? a.subMenus.push({interfaceCode: "", limitInfo: "", mask: "new", menuCode: "Sub.B2cOrder", menuIcon: null, pageId: null, permissionId: 0, rank: "15", subMenuName: "B2C订单管理", url: "/page/medicine/e/b2cOrder"}) : !0;
             var n = a.subMenus || [];
