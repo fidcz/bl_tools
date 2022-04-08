753dbeb058d8426362136fe2ca8704fecb4bcaa4
fidcz <fidcz123@163.com>
2022-04-08 15:43:41 +0800

    更新文件 tampermonkey.js

--- js/yiyao.meituan.com/tampermonkey.js
+++ js/yiyao.meituan.com/tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         美团替换页面和声音次数
 // @namespace    mt_change
-// @version      0.11
+// @version      0.12
 // @description  美团替换页面和声音次数
 // @author       fidcz
 // @include      *yiyao.meituan.com/*
@@ -42,7 +42,39 @@
         requirejs.config(a);
     }
 
+        function sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    function changeEle(id, loop, ogg){
+        var ele = document.getElementById(id);
+        //console.log(ele);
+        if(ele == undefined){return;}
+        //console.log('<div id="' + id + '" data-loop="' + loop + '" data-src="' + ogg + '"></div>');
+        ele.setAttribute("data-loop", loop);
+        ele.setAttribute("data-src", ogg);
+
+        //ele.innerHTML='<div id="' + id + '" data-loop="' + loop + '" data-src="' + ogg + '"></div>';
+    }
+    console.log('Change Require');
+
+    async function setLoop() {
+        for(let i=0;i<=20;i++){
+            //$("#refund-audio").attr("data-loop", "1");
+            //$("#new-order-audio").attr("data-src", "newOrderx.ogg");
+            //$("#reminder-audio").attr("data-loop", "1");
+            changeEle("refund-audio", "1", "refundSound1.ogg");
+            changeEle("reminder-audio", "1", "reminderSound1.ogg");
+            await sleep(500);
+        }
+            console.log("Auto change end");
+    }
+    console.log("Start modify loop");
+
     window.onload = ()=>{
+        changeEle("refund-audio", "1", "refundSound1.ogg");
+        changeEle("reminder-audio", "1", "reminderSound1.ogg");
+        setLoop();
     }
 
 
