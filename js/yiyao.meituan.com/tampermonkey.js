e91d631e73fa80154b36202ba66c0d9a96fc6b3b
blComputer <fidcz123@163.com>
2022-05-27 16:40:53 +0800

    添加桌面通知

--- js/yiyao.meituan.com/tampermonkey.js
+++ js/yiyao.meituan.com/tampermonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         美团替换页面和声音次数
 // @namespace    mt_change
-// @version      0.23
-// @description  美团替换页面和声音次数0.23
+// @version      0.24
+// @description  美团替换页面和声音次数0.24
 // @author       fidcz
 // @include      *yiyao.meituan.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=meituan.com
@@ -18,7 +18,7 @@
     // 历史订单页面: https://yiyao.meituan.com/#/v2/order/history
     // 退款订单页面: https://yiyao.meituan.com/#/v2/order/refund/unprocessed
     // 催单订单页面: https://yiyao.meituan.com/#/v2/order/reminder
-    console.log('mt_change ver:0.23');
+    console.log('mt_change ver:0.24');
 
 
     // 使用本地网站上的version
@@ -48,7 +48,8 @@
         '/static/' + jsVersion + '/js/page/order/search.js',
         '/static/' + jsVersion + '/js/page/order/reminder/order_reminder.js',
         '/static/' + jsVersion + '/js/page/order/history.js',
-        '/static/' + jsVersion + '/js/page/order/refund/unprocessed.js'
+        '/static/' + jsVersion + '/js/page/order/refund/unprocessed.js',
+        '/static/' + jsVersion + '/js/lib/dxSDK.src.extra.min.js'
         ]
 
     require = function(a,b,c,d){
